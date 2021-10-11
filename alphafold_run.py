c179d2c7718ac03e8375fab1b0fc571a58facaf9
Yuntao Yang <47754416+yuntaoyang@users.noreply.github.com>
2021-10-11 10:05:09 -0500

    Update alphafold_run.py

--- alphafold_run.py
+++ alphafold_run.py
@@ -62,7 +62,7 @@ files = os.listdir(path_input)
 logger.info("alphafold start!")
 for file in files:
     alphafold = 'python'+' '+alphafold+' '                '--fasta_paths'+' '+path_input+file+' '+                '--max_template_date='+max_template_date+' '+                '--gpu_devices'+' '+gpu+' '+                '--output_path='+path_output+' '+                '>'+' '+path_output+file.replace('.fasta','')+'.log'+' '+'2>&1'
-    process = subprocess.Popen(samtools_index,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    process = subprocess.Popen(alphafold,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     process.communicate()
     logger.info(file+" is done!")
 
